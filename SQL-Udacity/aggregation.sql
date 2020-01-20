5aad5912f19cd66e9b82d98d968a840e0fab1533
DAOU Otmane <pxotmane@gmail.com>
2020-01-20 12:37:29 +0100

    20-01-2019 changes

--- SQL-Udacity/aggregation.sql
+++ SQL-Udacity/aggregation.sql
@@ -29,6 +29,9 @@ ORDER BY total_amt_usd DESC
 LIMIT 2;
 /*Since there are 6912 orders - we want the average of the 3457 and 3456 order amounts when ordered. This is the average of 2483.16 and 2482.55. This gives the median of 2482.855. This obviously isn't an ideal way to compute. If we obtain new orders, we would have to change the limit. SQL didn't even calculate the median for us. The above used a SUBQUERY, but you could use any method to find the two necessary values, and then you just need the average of them.*/
 
+/**********************************/
+/****GROUP BY Part I exercices****/
+/********************************/
 /*Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.*/
 SELECT a.name, o.occurred_at
 FROM orders o
@@ -62,7 +65,8 @@ SELECT a.name AS NameACCOUNT, w.channel AS WebCHANNEL, w.occurred_at AS OccuDATE
 FROM accounts a
 JOIN web_events w
 ON a.id = w.account_id
-ORDER BY OccuDATE
+ORDER BY OccuDATE /*DESC*/
+/*LIMIT 1*/
 /*Udacity solution*/
 SELECT w.occurred_at, w.channel, a.name
 FROM web_events w
@@ -124,3 +128,73 @@ JOIN sales_reps s
 ON r.id = s.region_id
 GROUP BY r.name
 ORDER BY num_reps;
+
+/***********************************/
+/****GROUP BY Part II exercices****/
+/*********************************/
+/*For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns - one for the account name and one for the average quantity purchased for each of the paper types for each account.*/
+SELECT a.name AS AccNAME, AVG(o.standard_qty) AS std_avg_cmd, AVG(o.gloss_qty) AS gloss_avg_cmd, AVG(o.poster_qty) AS poster_avg_cmd
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY AccNAME
+ORDER BY AccNAME
+/*Udacity solution*/
+SELECT a.name, AVG(o.standard_qty) avg_stand, AVG(o.gloss_qty) avg_gloss, AVG(o.poster_qty) avg_post
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+/*For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type.*/
+SELECT a.name AS AccNAME, AVG(o.standard_amt_usd) AS std_avg_amt, AVG(o.gloss_amt_usd) AS gloss_avg_amt, AVG(o.poster_amt_usd) AS post_avg_amt
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY AccNAME
+ORDER BY AccNAME
+/*Udacity solution*/
+SELECT a.name, AVG(o.standard_amt_usd) avg_stand, AVG(o.gloss_amt_usd) avg_gloss, AVG(o.poster_amt_usd) avg_post
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+/*Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.*/
+SELECT s.name AS SalesRepsNAME, w.channel AS NameCHANNEL, COUNT(w.occurred_at) AS OccurTIMES
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN web_events w
+ON w.account_id = a.id
+GROUP BY SalesRepsNAME, NameCHANNEL
+ORDER BY OccurTIMES DESC, NameCHANNEL, SalesRepsNAME
+/*Udacity solution*/
+SELECT s.name, w.channel, COUNT(*) num_events
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name, w.channel
+ORDER BY num_events DESC;
+
+/*Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.*/
+SELECT r.name AS NameREGION, w.channel AS NameCHANNEL, COUNT(w.occurred_at) AS OccurTIMES
+FROM region r
+JOIN sales_reps s ON s.region_id = r.id
+JOIN accounts a ON a.sales_rep_id = s.id
+JOIN web_events w ON w.account_id = a.id
+GROUP BY NameREGION, NameCHANNEL
+ORDER BY OccurTIMES DESC, NameREGION, NameCHANNEL
+/*Udacity solution*/
+SELECT r.name, w.channel, COUNT(*) num_events
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name, w.channel
+ORDER BY num_events DESC;
