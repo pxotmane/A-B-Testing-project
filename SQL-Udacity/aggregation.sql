37deac87412954f8f071e39b5e82d1bdb766f2c9
DAOU Otmane <pxotmane@gmail.com>
2020-01-21 22:50:17 +0100

    21/01/2020 at 22:50 changes

--- SQL-Udacity/aggregation.sql
+++ SQL-Udacity/aggregation.sql
@@ -417,3 +417,88 @@ ON a.id = w.account_id
 GROUP BY a.id, a.name, w.channel
 ORDER BY use_of_channel DESC
 LIMIT 10;
+
+/***********************/
+/****DATE exercices****/
+/*********************/
+/* 1-Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. Do you notice any trends in the yearly sales totals?*/
+SELECT DATE_TRUNC('year', occurred_at), SUM(total_amt_usd)
+FROM orders
+GROUP BY 1
+ORDER BY 1 DESC
+/*Udacity solution*/
+SELECT DATE_PART('year', occurred_at) ord_year,  SUM(total_amt_usd) total_spent
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC;
+
+/* 2-Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset?*/
+SELECT DATE_PART('month', occurred_at), SUM(total_amt_usd)
+FROM orders
+GROUP BY 1
+ORDER BY 1 DESC
+/*Udacity solution*/
+SELECT DATE_PART('month', occurred_at) ord_month, SUM(total_amt_usd) total_spent
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC;
+
+/* 3-Which year did Parch & Posey have the greatest sales in terms of total number of orders? Are all years evenly represented by the dataset?*/
+SELECT DATE_PART('year', occurred_at), SUM(total)
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC
+/*Udacity solution*/
+SELECT DATE_PART('year', occurred_at) ord_year,  COUNT(*) total_sales
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC;
+
+/* 4-Which month did Parch & Posey have the greatest sales in terms of total number of orders? Are all months evenly represented by the dataset?*/
+SELECT DATE_PART('month', occurred_at), SUM(total)
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC
+/*Udacity solution*/
+SELECT DATE_PART('month', occurred_at) ord_month, COUNT(*) total_sales
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC;
+
+/* 5-In which month of which year did Walmart spend the most on gloss paper in terms of dollars?*/
+SELECT DATE_TRUNC('month', occurred_at) AS date_order, SUM(gloss_qty) AS GlossQty, a.name AS NameACCOUNT
+FROM accounts a
+JOIN orders o ON o.account_id = a.id
+GROUP BY date_order, NameACCOUNT
+HAVING a.name = 'Walmart'
+ORDER BY GlossQty DESC
+LIMIT 1
+/*Udacity solution*/
+SELECT DATE_TRUNC('month', o.occurred_at) ord_date, SUM(o.gloss_amt_usd) tot_spent
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+WHERE a.name = 'Walmart'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+/***********************/
+/****CASE exercices****/
+/*********************/
+/* 1-Write a query to display for each order, the account ID, total amount of the order, and the level of the order - ‘Large’ or ’Small’ - depending on if the order is $3000 or more, or smaller than $3000.*/
+
+/* 2-Write a query to display the number of orders in each of three categories, based on the total number of items in each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.*/
+
+/* 3-We would like to understand 3 different levels of customers based on the amount associated with their purchases. The top level includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd. The second level is between 200,000 and 100,000 usd. The lowest level is anyone under 100,000 usd. Provide a table that includes the level associated with each account. You should provide the account name, the total sales of all orders for the customer, and the level. Order with the top spending customers listed first.*/
+
+
+/*04-We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending customers listed first.*/
+
+
+/* 5-We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders. Create a table with the sales rep name, the total number of orders, and a column with top or not depending on if they have more than 200 orders. Place the top sales people first in your final table.*/
+
+
+/* 6-The previous didn't account for the middle, nor the dollar amount associated with the sales. Management decides they want to see these characteristics represented as well. We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales. The middle group has any rep with more than 150 orders or 500000 in sales. Create a table with the sales rep name, the total number of orders, total sales across all orders, and a column with top, middle, or low depending on this criteria. Place the top sales people based on dollar amount of sales first in your final table. You might see a few upset sales people by this criteria!*/
