74597e9d30d907135ef1950efed52ea6f3cd2c39
DAOU Otmane <pxotmane@gmail.com>
2020-01-24 01:49:52 +0100

    24-01-2020 at 01:49

--- SQL-Udacity/subquery.sql
+++ SQL-Udacity/subquery.sql
@@ -1,6 +1,7 @@
+/*> (greater than), < (less than), >= (greater than or equal to), <= (less than or equal to), = (equal to), != (not equal to)*/
+/*******************************************/
+/****Solutions to Your First Subquery 1****/
 /*****************************************/
-/****Solutions to Your First Subquery****/
-/***************************************/
 /* 1-First, we needed to group by the day and channel. Then ordering by the number of events (the third column) gave us a quick way to answer the first question.*/
 SELECT DATE_TRUNC('day',occurred_at) AS day,
    channel, COUNT(*) as events
@@ -23,3 +24,21 @@ FROM (SELECT DATE_TRUNC('day',occurred_at) AS day,
       GROUP BY 1,2) sub
 GROUP BY channel
 ORDER BY 2 DESC;
+/*******************************************/
+/****Solutions to Your First Subquery 2****/
+/*****************************************/
+/* 1-Here is the necessary quiz to pull the first month/year combo from the orders table.*/
+SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min_occur
+FROM orders
+
+/* 2-Then to pull the average for each, we could do this all in one query, but for readability, I provided two queries below to perform each separately.*/
+
+SELECT AVG(standard_qty) avg_std, AVG(gloss_qty) avg_gls, AVG(poster_qty) avg_pst
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) =
+     (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+
+SELECT SUM(total_amt_usd)
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) =
+      (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
