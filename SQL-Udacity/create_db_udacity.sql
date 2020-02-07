b4a7ed030759fb4882db143a4e5182c36bf2f239
DAOU Otmane <pxotmane@gmail.com>
2020-02-07 12:59:13 +0100

    07-02-2020 at 12:58 changes and add create file-tables-exemple.sql

--- SQL-Udacity/create_db_udacity.sql
+++ SQL-Udacity/create_db_udacity.sql
@@ -7,5 +7,28 @@ web_events: id	account_id	occurred_at	channel
 */
 
 /* database creation*/
-Create database 'Udacity_db'
-USE 'Udacity_db'
+Create database 'Udacity_db';
+USE 'Udacity_db';
+
+CREATE TABLE 'accounts'(
+  'id' INT(11) NOT NUll,
+  'name' VARCHAR(250) NOT NULL,
+  'website' VARCHAR(250) NOT NULL,
+  'lat' DECIMAL(2,8) NOT NULL,
+  'long' DECIMAL(2,8) NOT NULL,
+  'primary_poc' VARCHAR(2,8) NOT NULL,
+  'sales_rep_id' INT(8) NOT NULL,
+  PRIMARY KEY('id'),
+  KEY `sales_rep_id` (`sales_rep_id`),
+  CONSTRAINT `sales_reps_ibfk_1` FOREIGN KEY (`sales_rep_id`) REFERENCES `sales_reps` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT, CHARSET = latin1;
+
+CREATE TABLE 'orders' (
+  'id' INT(11) NOT NULL AUTO_INCREMENT,
+  'account_id' INT(11) NOT NULL,
+  'occurred_at' DATETIME NOT NULL,
+  'standard_qty' INT(11),
+  'gloss_qty' INT(11)
+)
