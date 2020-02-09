df7a4b13ddc3a18f66e7a211139afb7a82dda892
DAOU Otmane <pxotmane@gmail.com>
2020-02-09 23:12:39 +0100

    09-02-2020 changes

--- SQL-Udacity/create_db_udacity.sql
+++ SQL-Udacity/create_db_udacity.sql
@@ -7,28 +7,69 @@ web_events: id	account_id	occurred_at	channel
 */
 
 /* database creation*/
-Create database 'Udacity_db';
-USE 'Udacity_db';
+Create database Udacity_db;
+USE Udacity_db;
 
-CREATE TABLE 'accounts'(
-  'id' INT(11) NOT NUll,
-  'name' VARCHAR(250) NOT NULL,
-  'website' VARCHAR(250) NOT NULL,
-  'lat' DECIMAL(2,8) NOT NULL,
-  'long' DECIMAL(2,8) NOT NULL,
-  'primary_poc' VARCHAR(2,8) NOT NULL,
-  'sales_rep_id' INT(8) NOT NULL,
-  PRIMARY KEY('id'),
-  KEY `sales_rep_id` (`sales_rep_id`),
-  CONSTRAINT `sales_reps_ibfk_1` FOREIGN KEY (`sales_rep_id`) REFERENCES `sales_reps` (`id`)
+CREATE TABLE accounts(
+  id INT NOT NUll,
+  name VARCHAR(250) NOT NULL,
+  website VARCHAR(250) NOT NULL,
+  lat DECIMAL(10,8) NOT NULL,
+  long DECIMAL(10,8) NOT NULL,
+  primary_poc VARCHAR(255) NOT NULL,
+  sales_rep_id INT(8) NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT sales_reps_ibfk_1 FOREIGN KEY (sales_rep_id) REFERENCES sales_reps (id)
   ON UPDATE CASCADE
   ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT, CHARSET = latin1;
+) ENGINE = InnoDB, CHARSET = latin1;
 
-CREATE TABLE 'orders' (
-  'id' INT(11) NOT NULL AUTO_INCREMENT,
-  'account_id' INT(11) NOT NULL,
-  'occurred_at' DATETIME NOT NULL,
-  'standard_qty' INT(11),
-  'gloss_qty' INT(11)
-)
+CREATE TABLE orders (
+  id INT NOT NULL AUTO_INCREMENT,
+  account_id INT NOT NULL,
+  occurred_at DATETIME NOT NULL,
+  standard_qty INT,
+  gloss_qty INT,
+  poster_qty INT,
+  total INT,
+  standard_amt_usd FLOAT(6,2),
+  gloss_amt_usd FLOAT(6,2),
+  poster_amt_usd FLOAT(6,2),
+  total_amt_usd FLOAT(6,2),
+  PRIMARY KEY (id),
+  CONSTRAINT account_id_ibfk_1 FOREIGN KEY (account_id)
+  REFERENCES accounts (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+) ENGINE = InnoDB, CHARSET = latin1;
+
+DROP TABLE IF EXISTS web_events;
+CREATE TABLE web_events (
+  id int NOT NULL AUTO_INCREMENT,
+  account_id INT NOT NULL,
+  occurred_at DATETIME NOT NULL,
+  channel VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT account_id_ibfk_2 FOREIGN KEY(account_id)
+  REFERENCES accounts(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+) ENGINE = InnoDB, CHARSET = latin1;
+
+
+CREATE TABLE sales_reps (
+  id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  region_id INT NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT region_id_ibkf_1 FOREIGN KEY (region_id)
+  REFERENCES region (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+) ENGINE = InnoDB, CHARSET = latin1;
+
+CREATE TABLE region (
+  id INT NOT NUll AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE = InnoDB, CHARSET = latin1
